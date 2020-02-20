45f07bc5263337844043cdd40eef14c808eb76a3
Oscar Villarreal <thescarred@outlook.com>
2020-02-19 19:55:53 -0600

    Practica 1
    dia de 19/02/2020

---
+++ Assets/Scripts/Girl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Girl : MonoBehaviour
+{
+    public Girl otherGirl;
+    public TMPro.TMP_InputField mass;
+    public TMPro.TMP_InputField move;
+    Rigidbody rigi;
+    
+    void Start()
+    {
+        rigi = GetComponent<Rigidbody>();
+    }
+
+    public void SetUpdate()
+    {
+        if (otherGirl.mass.text == "" || otherGirl.move.text == "")
+        {
+           Debug.Log()
+
+            rigi.mass = float.Parse(mass.text);
+
+            if (otherGirl.mass.text == "")
+                otherGirl.mass.text = (rigi.mass * float.Parse(move.text) / float.Parse(otherGirl.move.text)).ToString();
+            else
+                otherGirl.move.text = (rigi.mass * float.Parse(move.text) / float.Parse(otherGirl.mass.text)).ToString();
+        }
+
+    }
+}
