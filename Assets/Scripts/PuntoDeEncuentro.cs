f1290544f91bb4b15bafd7f853b59d30b528bb1c
Oscar Villarreal <thescarred@outlook.com>
2020-03-11 19:56:43 -0600

    PRACTICA EXAMEN PUNTO DE ENCUENTRO
    AAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ Assets/Scripts/PuntoDeEncuentro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuntoDeEncuentro : MonoBehaviour
+{
+    public InputField v1, v2, p;
+    public movement p1, p2;
+
+    public void Calculate()
+    {
+        float vel1 = float.Parse(v1.text);
+        float vel2 = float.Parse(v2.text);
+        float t = 500 / (vel1 + vel2);
+        float x1 = -250 + (vel1 * t);
+        float x2 = -250 + (vel2 * t);
+
+        p.text = x1.ToString();
+
+        p1.Move(t, x1, vel1, true);
+        p2.Move(t, x2, vel2, false);
+
+        Debug.Log("x1f: " + x1  + "  " + "x2f: " + x2  + "  t: " + t);
+    }
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    Vector3 start, end;
+    float startTime, l, t, x, vel;
+    bool go = false;
+    bool moveRight;
+
+    public void Move(float _t, float _x, float _vel, bool mR) 
+    {
+        moveRight = mR;
+        start = transform.position;
+        if (mR)
+            end = new Vector3(_x, -50, 250);
+        else
+            end = new Vector3(-_x, -50, 250);
+        startTime = Time.time;
+        t = _t;
+        x = _x;
+        vel = _vel;
+        l = Vector3.Distance(transform.position, end);
+        go = true;
+
+        Debug.Log(gameObject.name + " " + end);
+    }
+    void Update()
+    {
+        if (go) 
+        {
+            float distCovered = (Time.time - startTime) * vel;
+            float fraccion = distCovered / l;
+            transform.position = Vector3.Lerp(start, end, fraccion);
+        }
+    }
+}
