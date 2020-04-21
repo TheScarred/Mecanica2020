101c065bda5eeb61b374a59b082180384d9ccf24
TheScarred <thescarred@outlook.com>
2020-04-20 22:39:06 -0500

    New Scene
    z

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public Rigidbody ball;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ball.velocity = Vector3.right * 80;
+    }
+}
