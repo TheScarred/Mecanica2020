ab6b08b6a8c6463a152959cabbd933c4fb169f8b
TheScarred <thescarred@outlook.com>
2020-04-22 20:38:22 -0500

    part1
    step

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -4,10 +4,62 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
-    public Rigidbody ball;
+    private float g = -9.81f, yI, yF, d, t;
 
-    private void OnCollisionEnter(Collision collision)
+    // los BP no funcionan en mi visual no se porque
+
+    private void Start()
     {
-        ball.velocity = Vector3.right * 80;
+        yI = transform.position.y;
+        StartCoroutine(ShootRay());
+        t = Mathf.Abs(CalculateTime(d));
+        StartCoroutine(Move(transform.position, new Vector3(transform.position.x, transform.position.y - d, transform.position.z), t));
     }
+
+    IEnumerator ShootRay()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+        {
+            Debug.Log("Ray hit");
+            d = Mathf.Abs(hit.distance - transform.localScale.x / 2);
+        }
+        else
+            Debug.Log("No ray hit");
+
+        yield return null;
+    }
+
+    IEnumerator Move(Vector3 a, Vector3 b, float t)
+    {
+        float lerpValue = 0.0f;
+
+        while (lerpValue < t)
+        {
+            transform.position = Vector3.Lerp(a, b, lerpValue);
+
+            lerpValue += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = b;
+
+
+        yield return null;
+    }
+
+    float CalculateTime(float d)
+    {
+        return d / g;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        
+    }
+
+/*
+    
+*/
 }
